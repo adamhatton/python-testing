07c49f716a5a71f4abe117aa38e5fa10d4159fa3
adamhatton <adamdhatton@gmail.com>
2022-06-20 18:35:25 +0000

    Add code for mocking tests

--- test_student.py
+++ test_student.py
@@ -1,5 +1,7 @@
 import unittest
 from student import Student
+from datetime import date, timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
 
@@ -20,6 +22,30 @@ class TestStudent(unittest.TestCase):
     
     def test_email(self):
         self.assertEqual(self.student.email, "adam.hatton@email.com")
+
+    
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            mocked_get.return_value.text = "Something went wrong with the request!"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 if __name__ == '__main__':
     unittest.main()
